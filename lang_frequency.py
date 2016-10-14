c9b9ed0606438b2b700776f1ee16c215a98567d1
k.kuzmin <k.kuzmin@npo-echelon.ru>
2016-10-14 19:32:22 +0300

    word frequency done

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,24 @@
+import argparse
 
 
 def load_data(filepath):
-    pass
+    with open(filepath, 'r') as text:
+        return text.read()
 
 
 def get_most_frequent_words(text):
-    pass
+    words = set(text.split(' '))
+    count_words = []
+    for word in words:
+        if word.isalpha():
+            count_words.append([word, text.count(word)])
+    return sorted(count_words, key=lambda x: x[1], reverse=True)[:9]
 
 
 if __name__ == '__main__':
-    pass
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-p', '--path', help="Path to file", required=True)
+    args = parser.parse_args()
+
+    data = load_data(args.path)
+    print(get_most_frequent_words(data))
