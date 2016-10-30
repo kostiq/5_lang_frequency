362493f6eccca976e6fb994f57e61a43df07d544
k.kuzmin <k.kuzmin@npo-echelon.ru>
2016-10-30 12:43:13 +0300

    готово для проверки

--- lang_frequency.py
+++ lang_frequency.py
@@ -9,7 +9,7 @@ def load_data(filepath):
 
 
 def get_most_frequent_words(text):
-    words = re.findall(r'\w+',text.lower())    
+    words = re.findall(r'\w+', text.lower())
     c = collections.Counter(words)
     top_10_word = 10
     return c.most_common(top_10_word)
